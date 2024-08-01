19e52ae3c4ab0a595d800c287ff7c3d6e6aefa31
BangNopall <craft6254@gmail.com>
2024-08-01 17:31:09 +0700

    fix bug

--- admin/bank_act.php
+++ admin/bank_act.php
@@ -5,5 +5,5 @@ $pemilik  = $_POST['pemilik'];
 $nomor  = $_POST['nomor'];
 $saldo  = $_POST['saldo'];
 
-mysqli_query($koneksi, "insert into bank values (NULL,'$nama','$pemilik','$nomor','$saldo')");
+mysqli_query($koneksi, "insert into bank values (NULL,'$nama','$nomor','$pemilik','$saldo')");
 header("location:bank.php");
